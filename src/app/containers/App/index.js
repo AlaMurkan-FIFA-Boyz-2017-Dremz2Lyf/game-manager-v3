08094a6181bc3d59ed56e6254e789422a604a03d
Scott <scotthorn0@gmail.com>
2017-10-13 19:55:21 -0500

    Remove inaccurate comment from App container

--- src/app/containers/App/index.js
+++ src/app/containers/App/index.js
@@ -5,7 +5,6 @@ const App = ({ children }) => (
   <main>
     <h1>Welcome to the main app mock</h1>
     <p>This wrapper app is used for dev to render the micro app.</p>
-    {/* Right now children is just a placeholder for web component Alex is working on */}
     {children}
   </main>
 );
