dbea9cf860608f42e1f26eaf59a12d283b0531ad
Scott <scotthorn0@gmail.com>
2018-04-18 17:34:50 -0500

    Add tests for utils

--- src/app/utilities/test/index.test.js
+++ src/app/utilities/test/index.test.js
@@ -1,13 +1,11 @@
-import { expect } from 'chai';
-
 import * as utils from '../index';
 
 describe('Utilities', () => {
   describe('processStats', () => {
     it('should take a string and return an object with home and away keys', () => {
-      const result = utils.processStats('');
-      expect(result).to.be.an('object');
-      expect(result).to.have.all.keys(['home', 'away']);
+      const result = utils.processStats(['']);
+      expect(result).toHaveProperty('home', { '': NaN });
+      expect(result).toHaveProperty('away', { '': NaN });
     });
   });
 });
