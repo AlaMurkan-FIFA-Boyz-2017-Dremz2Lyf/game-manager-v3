b025151a6d41b6e667ab8f2300458234557b9c81
Scott <scotthorn0@gmail.com>
2018-04-18 18:15:38 -0500

    Add tests for Searching component.

---
+++ src/app/components/FinishGame/tests/Searching.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Searching from '../Searching';
+
+describe('Searching', () => {
+  let wrapper;
+  let fetchImagesMock;
+
+  beforeEach(() => {
+    fetchImagesMock = jest.fn();
+  });
+
+  afterEach(() => {
+    fetchImagesMock.mockRestore();
+  });
+
+  it('should match the snapshot when loading', () => {
+    wrapper = shallow(
+      <Searching loading fetchImages={fetchImagesMock} />,
+    );
+
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('Loader').length).toBe(1);
+    expect(fetchImagesMock).toHaveBeenCalled();
+  });
+
+  it('should match the snapshot when loaded', () => {
+    wrapper = shallow(
+      <Searching loading={false} fetchImages={fetchImagesMock} />,
+    );
+
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('Loader').length).toBe(0);
+    expect(fetchImagesMock).toHaveBeenCalled();
+  });
+});
