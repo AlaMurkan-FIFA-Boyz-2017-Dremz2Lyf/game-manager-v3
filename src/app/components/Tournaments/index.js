833fa848234ee228bb007237c15558eb4f90217e
Scott <scotthorn0@gmail.com>
2018-04-18 17:30:30 -0500

    Add Static Tournaments Component.

--- src/app/components/Tournaments/index.js
+++ src/app/components/Tournaments/index.js
@@ -1,31 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Segment, Header, Button, Menu, Icon } from 'semantic-ui-react';
+import { Segment, Header, Container, Button, Item, Grid } from 'semantic-ui-react';
 
 const Tournaments = ({ onSelect }) => (
-  <Segment.Group>
-    <Segment>
-      <Header size="medium">
-        Tournaments
-      </Header>
-      <Button>
-        <Icon name="plus" />
-        New
-      </Button>
-    </Segment>
-    <Segment>
-      <Menu.Item>
-        Tournament Name
-        <Button onClick={onSelect} size="small">Play</Button>
-        Tournament status: created/started/finished
-      </Menu.Item>
-      <Menu.Item>
-        Tournament Name
-        <Button size="small">Play</Button>
-        Tournament status: created/started/finished
-      </Menu.Item>
-    </Segment>
-  </Segment.Group>
+  <Container text>
+    <Segment.Group>
+      <Segment>
+        <Grid columns={2} centered>
+          <Grid.Row>
+            <Grid.Column floated="left" textAlign="center">
+              <Header icon="trophy" content="Tournaments" />
+            </Grid.Column>
+            <Grid.Column floated="right" textAlign="right">
+              <Button icon="plus" content="New" />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Segment>
+        <Item.Group>
+          <Item>
+            <Item.Content>
+              <Item.Header>
+                Super great Tournament
+              </Item.Header>
+              <Button floated="right" onClick={onSelect} size="small">Play</Button>
+              <Item.Extra>
+                Tournament status: started
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+          <Item>
+            <Item.Content>
+              <Item.Header>
+                Less great Tournament
+              </Item.Header>
+              <Button floated="right" size="small">Play</Button>
+              <Item.Extra>
+                Tournament status: created
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Segment>
+    </Segment.Group>
+  </Container>
 );
 
 Tournaments.propTypes = {
--- src/app/components/Tournaments/tests/index.test.js
+++ src/app/components/Tournaments/tests/index.test.js
@@ -1,12 +1,28 @@
-import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import React from 'react';
 
 import Tournaments from '../index';
 
 describe('<Tournaments />', () => {
+  let onSelect;
+  let wrapper;
+
+  beforeEach(() => {
+    onSelect = jest.fn();
+  });
+
+  afterEach(() => {
+    onSelect.mockRestore();
+  });
+
   it('should render', () => {
-    const renderedComponent = shallow(<Tournaments />);
-    expect(renderedComponent).to.be.present();
+    wrapper = shallow(<Tournaments />);
+    expect(wrapper).toBeDefined();
+  });
+
+  it('should call the onSelect prop when the play button is clicked', () => {
+    wrapper = shallow(<Tournaments onSelect={onSelect} />);
+    wrapper.find('Button').at(1).simulate('click');
+    expect(onSelect).toHaveBeenCalled();
   });
 });
